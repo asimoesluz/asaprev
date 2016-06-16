43a09b8bcf2f93b628ebb9b7b7ac6e94b9427dca
asimoesluz <asimoesluz@gmail.com>
2016-06-16 16:10:13 -0300

    atualzaþ§es

--- addCommit.bat
+++ addCommit.bat
@@ -1,4 +1,5 @@
 @echo off
+rem adicionar & confirmar
 set path=%path%;C:\Program Files\Git\bin
 git add *
 git commit -m "atualzações"
---
+++ atualizar.bat
@@ -0,0 +1,5 @@
+@echo off
+rem Para enviar estas alterações ao seu repositório remoto, execute 
+set path=%path%;C:\Program Files\Git\bin
+git push origin master
+pause
